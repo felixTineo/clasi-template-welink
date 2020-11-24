0ad7fb4ebd2c78d69f4d0cfc539d26be116453eb
felix tineo <ftandcompany@gmail.com>
2020-11-24 11:07:18 -0400

    seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,29 @@ require('dotenv').config({
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Elephant | inmobiliaria",
+    author: "Clasihome",
+    //imageUrl: "https://clasihome.com/properties/back.png",
+    description:
+      "Conectamos clientes con propiedades. Optimiza, gestiona y vende más. La nueva plataforma inmobiliaria que te ayuda a optimizar tu tiempo de trabajo y obtener mejores resultados. Pruébalo gratis por 15 días.",
+    //keywords: `crm, inmobilario, chile, republica, propiedades, republicar, portales, portal, inmobiliaria, software, web, santiago, corredor, casas, departamentos, venta, arriendo, mejores, precios, app`,
+    facebook: "https://www.facebook.com/Clasihome-109624727580277/",
+    instagram: `https://www.instagram.com/_clasihome/?igshid=1u34cmwk5fudl`,
+    siteUrl: `https://tender-wozniak-faad50.netlify.app/`,
+  },  
   plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Elephant | inmobiliaria",
+        short_name: "elephant",
+        start_url: "/",
+        icon: "static/logo.svg",
+      },
+    },    
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
